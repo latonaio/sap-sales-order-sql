0fa33d221419d3cb14760c664bf36aad2e34b08c
Yurino Oga <yurino.o@latona.io>
2021-12-12 09:43:41 +0900

    release

---
+++ sap-sales-order-sql-item-schedule-line-data.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `sap-sales-order-item-schedule-line-data`
+(
+  'SalesOrder'                     varchar(10) DEFAULT NULL,
+  'SalesOrderItem'                 varchar(6) DEFAULT NULL,
+  'ScheduleLine'                   varchar(3) DEFAULT NULL,
+  'RequestedDeliveryDate'          date DEFAULT NULL,
+  'ConfirmedDeliveryDate'          date DEFAULT NULL,
+  'OrderQuantityUnit'              varchar(3) DEFAULT NULL,
+  'ScheduleLineOrderQuantity'      varchar(15) DEFAULT NULL,
+  'ConfdOrderQtyByMatlAvailCheck'  varchar(15) DEFAULT NULL,
+  'DeliveredQtyInOrderQtyUnit'     varchar(15) DEFAULT NULL,
+  'OpenConfdDelivQtyInOrdQtyUnit'  varchar(15) DEFAULT NULL,
+  'CorrectedQtyInOrderQtyUnit'     varchar(15) DEFAULT NULL,
+  'DelivBlockReasonForSchedLine'   varchar(2) DEFAULT NULL,
+    PRIMARY KEY (`SalesOrder`, `SalesOrderItem`, `ScheduleLine`)
+    CONSTRAINT `SalesOrder_fk` FOREIGN KEY (`SalesOrder`) REFERENCES `sap-sales-order-header-data` (`SalesOrder`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
