dd03f9517069fe3406da16d0cef05893471b6c71
Yurino Oga <yurino.o@latona.io>
2021-12-15 00:05:28 +0900

    release

--- sap-sales-order-sql-header-partner-data.sql
+++ sap-sales-order-sql-header-partner-data.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `sap-sales-order-header-partner-data`
 (
-  'SalesOrder'                   varchar(10) DEFAULT NULL,
-  'PartnerFunction'              varchar(2) DEFAULT NULL,
-  'Customer'                     varchar(10) DEFAULT NULL,
-  'Supplier'                     varchar(10) DEFAULT NULL,
+  `SalesOrder`                   varchar(10) DEFAULT NULL,
+  `PartnerFunction`              varchar(2) DEFAULT NULL,
+  `Customer`                     varchar(10) DEFAULT NULL,
+  `Supplier`                     varchar(10) DEFAULT NULL,
     PRIMARY KEY (`SalesOrder`)
     CONSTRAINT `SalesOrder_fk` FOREIGN KEY (`SalesOrder`) REFERENCES `sap-sales-order-header-data` (`SalesOrder`)
 ) ENGINE = InnoDB
